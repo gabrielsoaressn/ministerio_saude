83f34d1b4b7e2856ea8e5b71b22d3a47621d7392
Beatriz Saori Kyohara <72822906+Shiroiyume@users.noreply.github.com>
2021-04-21 23:25:49 -0300

    Add files via upload

---
+++ RetornaTodosInsumoDisponiveisTipoEstoqueMS.cpp
@@ -0,0 +1,42 @@
+void RetornaTodosInsumoDisponiveisTipoEstoqueMS(){
+
+     if (quantidadeTotalVacinasMS != 0){ //Para Vacina
+        cout << "----------------------------------------------------------\n" << endl;
+        cout << "Vacinas Disponiveis no Estoque do MS" << endl;
+        cout << "Quantidade: " << quantidadeTotalVacinasMS << "\n" << endl;
+        cout << "----------------------------------------------------------\n" << endl;
+
+    }else{
+        cout << "----------------------------------------------------------\n" << endl;
+        cout << "Vacinas estao Indisponiveis no Estoque do MS.\n" << endl;
+        cout << "----------------------------------------------------------\n" << endl;
+
+    }
+
+    if (quantidadeTotalEPIMS != 0){ //Para EPI
+
+        cout << "EPI Disponivel no Estoque do MS" << endl;
+        cout << "Quantidade: " << quantidadeTotalEPIMS << "\n" << endl;
+        cout << "----------------------------------------------------------\n" << endl;
+
+    }else{
+
+        cout << "EPI Indisponivel no Estoque do MS.\n" << endl;
+        cout << "----------------------------------------------------------\n" << endl;
+
+    }
+
+    if (quantidadeTotalMedicamentosMS != 0){ //Para Medicamento
+
+        cout << "Medicamentos Disponivel no Estoque do MS" << endl;
+        cout << "Quantidade: " << quantidadeTotalMedicamentosMS << "\n" << endl;
+        cout << "----------------------------------------------------------\n" << endl;
+
+    }else{
+
+        cout << "Medicamentos Indisponiveis no Estoque do MS.\n" << endl;
+        cout << "----------------------------------------------------------\n" << endl;
+
+    }
+
+}
