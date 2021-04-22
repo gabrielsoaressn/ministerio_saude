1217bfd8d198cdcfd7d9cc83860b6cf73fabb491
Beatriz Saori Kyohara <72822906+Shiroiyume@users.noreply.github.com>
2021-04-22 10:36:57 -0300

    Update structVacina.cpp

--- structVacina.cpp
+++ structVacina.cpp
@@ -5,8 +5,11 @@ typedef struct
 {
     std::string nome;
     std::string tipoVacina;
+    
+    //Saori Alterando aqui
     std::string fabricante;
     std::string dataVenc;
+    
     int quantidadeDoses;
     int intervaloDias;
     int quantidadeDvacina;
