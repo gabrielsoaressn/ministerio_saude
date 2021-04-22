17dddaf08d9bf9a18b22f9161253c9a15ee4682b
Beatriz Saori Kyohara <72822906+Shiroiyume@users.noreply.github.com>
2021-04-22 11:10:42 -0300

    Update ConsultaDescricaoInsumosDistribuidosParaEstados.cpp

--- ConsultaDescricaoInsumosDistribuidosParaEstados.cpp
+++ ConsultaDescricaoInsumosDistribuidosParaEstados.cpp
@@ -1,6 +1,16 @@
+#ifndef CONSULTADESCRICAOINSUMOSDISTRIBUIDOSPARAESTADO_CPP
+#define CONSULTADESCRICAOINSUMOSDISTRIBUIDOSPARAESTADO_CPP
+
+#include <iostream>
+#include <string>
+#include "transferirInsumo.cpp"
+
+using namespace std;
+
+
 void ConsultaDescricaoInsumosDistribuidosParaEstados(){
 
-    int y, descricao, n = 1, i;
+    int y, descricao, n = 1, i, j;
 
     cout << "----------------------------------------------------------\n" << endl;
     cout << "Essa funcao consulta as descricoes dos Insumos Distribuidos para os Estados\n" << endl;
@@ -13,9 +23,9 @@ void ConsultaDescricaoInsumosDistribuidosParaEstados(){
         if (y == 1){
             cout << "----------------------------------------------------------\n" << endl;
             cout << "Digite o Insumo que desejas consultar a descricao: " << endl;;
-            cout<<" 1) Vacina" << endl;
-            cout<<" 2) EPI" << endl;
-            cout<<" 3) Medicamento" << endl;
+            cout << " 1) Vacina" << endl;
+            cout << " 2) EPI" << endl;
+            cout << " 3) Medicamento" << endl;
             cin >> descricao;
 
             switch (descricao){
@@ -23,13 +33,20 @@ void ConsultaDescricaoInsumosDistribuidosParaEstados(){
                     if(quantidadeTotalVacinasEstados != 0){
                         cout << "----------------------------------------------------------\n" << endl;
                         cout << "Descricao das Vacinas: " << "\n" << endl;
-                        for (i = 0; i < 50; i++){
+
+                        for(j = 0; j < 27; ++j){
+                            for(i = 0; i < MAX_VACINAS; ++i ){
                                 if(vacinas[i].ativo == 1){
-                                    cout << "Tipo de vacina: "<< vacinas[i].tipoVacina << endl;
-                                    cout << "Dose da vacina: "<< vacinas[i].quantidadeDoses << endl;
-                                    cout << "Intervalo entre as doses: "<< vacinas[i].intervaloDias << endl;
+                                    cout << "Estado: " << estados[j].nome << endl;
+                                    cout << "Atributos: " << endl;
+                                    cout << "Tipo de vacina: " << estados[j].vacinasDoEstado[i].tipoVacina << endl;
+                                    cout << "Dose da vacina: " << estados[j].vacinasDoEstado[i].quantidadeDoses << endl;
+                                    cout << "Intervalo entre as doses: " << estados[j].vacinasDoEstado[i].intervaloDias << endl;
+                                    cout << "Fabricante: " << estados[j].vacinasDoEstado[i].fabricante << endl;
+                                    cout << "Data de Vencimento: " <<estados[j].vacinasDoEstado[i].dataVenc << endl;
                                     cout << "----------------------------------------------------------\n" << endl;
                                 }
+                            }
                         }
                         cout << "Desejas consultar mais alguma descricao?" << endl;
                         cout << "Digite 1 para Sim e 0 para Nao" << endl;
@@ -46,14 +63,20 @@ void ConsultaDescricaoInsumosDistribuidosParaEstados(){
                     }
                 }
                 case 2:{
-                    if(quantidadeTotalEPIEstados != 0){ //verEssaVariavel
+                    if(quantidadeTotalEPIEstados != 0){
                         cout << "----------------------------------------------------------\n" << endl;
-                        cout << "Descricao de EPI: " << "\n" << endl;
-                        for(i = 0; i < 50; i++){
-                            if(epis[i].ativo == 1){
-                                cout << "Tipo da EPI: " << epis[i].tipoEPI << endl;
-                                cout << "Descricao da EPI: " << epis[i].descricaoEPI << endl;
-                                cout << "----------------------------------------------------------\n" << endl;
+                        cout << "Descricao das EPIs: " << "\n" << endl;
+                        for(j = 0; j < 27; ++j){
+                            for(i = 0; i < MAX_EPI; ++i ){
+                                if(estados[j].episDoEstado[i].ativo == 1){
+                                    cout << "Estado: " << estados[j].nome << endl;
+                                    cout << "Atributos: " << endl;
+                                    cout << "Tipo da EPI: " << estados[j].episDoEstado[i].tipoEPI << endl;
+                                    cout << "Descricao da EPI: " << estados[j].episDoEstado[i].descricaoEPI << endl;
+                                    cout << "Fabricante: " << estados[j].episDoEstado[i].fabricante << endl;
+                                    cout << "Data de Vencimento: " <<estados[j].episDoEstado[i].dataVenc << endl;
+                                    cout << "----------------------------------------------------------\n" << endl;
+                                }
                             }
                         }
                         cout << "Desejas consultar mais alguma descricao?" << endl;
@@ -74,12 +97,19 @@ void ConsultaDescricaoInsumosDistribuidosParaEstados(){
                     if(quantidadeTotalMedicamentosEstados != 0){
                         cout << "----------------------------------------------------------\n" << endl;
                         cout << "Descricao dos Medicamentos: " << "\n" << endl;
-                        for (i = 0; i < 50; i++){
-                            if(medicamentos[i].ativo == 1){
-                                cout << "Disponibilizacao do medicamento: \n"<< medicamentos[i].disponibilizacao << endl;
-                                cout << "Administracao do medicamento: \n"<< medicamentos[i].administracao << endl;
-                                cout << "Dosagem do medicamento: \n"<< medicamentos[i].dosagem << endl;
-                                cout << "----------------------------------------------------------\n" << endl;
+                        for(j = 0; j < 27; ++j){
+                            for(i = 0; i < MAX_MEDICAMENTOS; ++i){
+                                if(medicamentos[i].ativo == 1){
+                                    cout << "Estado: " << estados[j].nome << endl;
+                                    cout << "Atributos: " << endl;
+                                    cout << "Nome do medicamento: "<< estados[j].medicamentosDoEstado[i].nome << endl;
+                                    cout << "Disponibilizacao do medicamento: "<< estados[j].medicamentosDoEstado[i].disponibilizacao << endl;
+                                    cout << "Administracao do medicamento: "<< estados[j].medicamentosDoEstado[i].administracao << endl;
+                                    cout << "Dosagem do medicamento: "<< estados[j].medicamentosDoEstado[i].dosagem << endl;
+                                    cout << "Fabricante: " << estados[j].medicamentosDoEstado[i].fabricante << endl;
+                                    cout << "Data de Vencimento: " <<estados[j].medicamentosDoEstado[i].dataVenc << endl;
+                                    cout << "----------------------------------------------------------\n" << endl;
+                                }
                             }
                         }
                         cout << "Desejas consultar mais alguma descricao?" << endl;
@@ -112,25 +142,37 @@ void ConsultaDescricaoInsumosDistribuidosParaEstados(){
             cout << "----------------------------------------------------------\n" << endl;
                 if(QuantidadeTotalVacinasEstados != 0){
                     cout << "Descricao das Vacinas: " << endl;
-                    for (i = 0; i < 50; i++){
-                        if(vacinas[i].ativo == 1){
-                            cout << "Tipo de vacina: "<< vacinas[i].tipoVacina << endl;
-                            cout << "Dose da vacina: "<< vacinas[i].quantidadeDoses << endl;
-                            cout << "Intervalo entre as doses: "<< vacinas[i].intervaloDias << endl;
-                            cout << "----------------------------------------------------------\n" << endl;
+                        for(j = 0; j < 27; ++j){
+                            for(i = 0; i < MAX_VACINAS; ++i ){
+                                if(vacinas[i].ativo == 1){
+                                    cout << "Estado: "<< estados[j].nome << endl;
+                                    cout << "Atributos: " << endl;
+                                    cout << "Tipo de vacina: " << estados[j].vacinasDoEstado[i].tipoVacina << endl;
+                                    cout << "Dose da vacina: " << estados[j].vacinasDoEstado[i].quantidadeDoses << endl;
+                                    cout << "Intervalo entre as doses: " << estados[j].vacinasDoEstado[i].intervaloDias << endl;
+                                    cout << "Fabricante: " << estados[j].vacinasDoEstado[i].fabricante << endl;
+                                    cout << "Data de Vencimento: " <<estados[j].vacinasDoEstado[i].dataVenc << endl;
+                                    cout << "----------------------------------------------------------\n" << endl;
+                                }
+                            }
                         }
-                    }
                 }else{
                     cout << "Indisponivel por falta no estoque.\n" << endl;
                     cout << "----------------------------------------------------------\n" << endl;
                 }
                 if(QuantidadeTotalEPIEstados != 0){
-                    cout << "Descricao das Vacinas: " << endl;
-                        for(i = 0; i < 50; i++){
-                            if(epis[i].ativo == 1){
-                                cout << "Tipo da EPI: " << epis[i].tipoEPI << endl;
-                                cout << "Descricao da EPI: " << epis[i].descricaoEPI << endl;
-                                cout << "----------------------------------------------------------\n" << endl;
+                    cout << "Descricao das EPIs: " << endl;
+                        for(j = 0; j < 27; ++j){
+                            for(i = 0; i < MAX_EPI; ++i ){
+                                if(estados[j].episDoEstado[i].ativo == 1){
+                                    cout << "Estado: " << estados[j].nome << endl;
+                                    cout << "Atributos: " << endl;
+                                    cout << "Tipo da EPI: " << estados[j].episDoEstado[i].tipoEPI << endl;
+                                    cout << "Descricao da EPI: " << estados[j].episDoEstado[i].descricaoEPI << endl;
+                                    cout << "Fabricante: " << estados[j].episDoEstado[i].fabricante << endl;
+                                    cout << "Data de Vencimento: " <<estados[j].episDoEstado[i].dataVenc << endl;
+                                    cout << "----------------------------------------------------------\n" << endl;
+                                }
                             }
                         }
                 }else{
@@ -139,19 +181,26 @@ void ConsultaDescricaoInsumosDistribuidosParaEstados(){
                     }
                 if(QuantidadeTotalMedicamentosEstados != 0){
                     cout << "Descricao dos Medicamentos: " << endl;
-                        for (i = 0; i < 50; i++){
-                            if(medicamentos[i].ativo == 1){
-                                cout << "Disponibilizacao do medicamento: \n"<< medicamentos[i].disponibilizacao << endl;
-                                cout << "Administracao do medicamento: \n"<< medicamentos[i].administracao << endl;
-                                cout << "Dosagem do medicamento: \n"<< medicamentos[i].dosagem << endl;
-                                cout << "----------------------------------------------------------\n" << endl;
+                        for(j = 0; j < 27; ++j){
+                            for(i = 0; i < MAX_MEDICAMENTOS; ++i){
+                                if(medicamentos[i].ativo == 1){
+                                    cout << "Estado: " << estados[j].nome << endl;
+                                    cout << "Atributos: " << endl;
+                                    cout << "Nome do medicamento: "<< estados[j].medicamentosDoEstado[i].nome << endl;
+                                    cout << "Disponibilizacao do medicamento: "<< estados[j].medicamentosDoEstado[i].disponibilizacao << endl;
+                                    cout << "Administracao do medicamento: "<< estados[j].medicamentosDoEstado[i].administracao << endl;
+                                    cout << "Dosagem do medicamento: "<< estados[j].medicamentosDoEstado[i].dosagem << endl;
+                                    cout << "Fabricante: " << estados[j].episDoEstado[i].fabricante << endl;
+                                    cout << "Data de Vencimento: " <<estados[j].episDoEstado[i].dataVenc << endl;
+                                    cout << "----------------------------------------------------------\n" << endl;
+                                }
                             }
                         }
                 }else{
                     cout << "Indisponivel por falta no estoque.\n" << endl;
                     cout << "----------------------------------------------------------\n" << endl;
-                    }
-
+                }
+            n = 0;
             break;
         }
 
@@ -163,3 +212,4 @@ void ConsultaDescricaoInsumosDistribuidosParaEstados(){
         }
     }
 }
+#endif
